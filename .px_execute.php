16ae8ffa5eb2d74c8d9599d9d9d31feed551cf96
Tomoya Koyanagi <tomk79@gmail.com>
2016-07-16 22:58:33 +0900

    (エラーを可視化)

--- .px_execute.php
+++ .px_execute.php
@@ -1,4 +1,6 @@
 <?php
 chdir(__DIR__);
+@ini_set('display_errors', 'On');
+@ini_set('error_reporting', 32767);
 @require_once( './vendor/autoload.php' );
 new picklesFramework2\pickles('./px-files/');
