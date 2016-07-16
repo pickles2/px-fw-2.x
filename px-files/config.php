0cb063d3a7a810a50c53cf8dab51d5b95fdd44aa
Tomoya Koyanagi <tomk79@gmail.com>
2016-07-16 15:31:00 +0900

    configのコメントを形式を変更

--- px-files/config.php
+++ px-files/config.php
@@ -1,52 +1,86 @@
 <?php
+/**
+ * config.php template
+ */
 return call_user_func( function(){
 
 	// initialize
+
+	/** コンフィグオブジェクト */
 	$conf = new stdClass;
 
+
 	// project
-	$conf->name = 'Pickles Framework 2'; // サイト名
-	$conf->copyright = 'Pickles 2 Project'; // コピーライト表記
-	$conf->domain = null; // ドメイン
-	$conf->path_controot = '/'; // コンテンツルートディレクトリ
+
+	/** サイト名 */
+	$conf->name = 'Pickles Framework 2';
+	/** コピーライト表記 */
+	$conf->copyright = 'Pickles 2 Project';
+	/** ドメイン(本番環境のドメイン) */
+	$conf->domain = null;
+	/** コンテンツルートディレクトリ */
+	$conf->path_controot = '/';
+
 
 	// paths
-	$conf->path_top = '/'; // トップページのパス(デフォルト "/")
-	$conf->path_publish_dir = './px-files/dist/'; // パブリッシュ先ディレクトリパス
-	$conf->public_cache_dir = '/caches/'; // 公開キャッシュディレクトリ
-	$conf->path_files = '{$dirname}/{$filename}_files/'; // リソースディレクトリ(各コンテンツに対して1:1で関連付けられる)のパス
-	$conf->contents_manifesto = '/common/contents_manifesto.ignore.php'; // Contents Manifesto のパス
 
-	// directory index
+	/** トップページのパス(デフォルト "/") */
+	$conf->path_top = '/';
+	/** パブリッシュ先ディレクトリパス */
+	$conf->path_publish_dir = './px-files/dist/';
+	/** 公開キャッシュディレクトリ */
+	$conf->public_cache_dir = '/caches/';
+	/** リソースディレクトリ(各コンテンツに対して1:1で関連付けられる)のパス */
+	$conf->path_files = '{$dirname}/{$filename}_files/';
+	/** Contents Manifesto のパス */
+	$conf->contents_manifesto = '/common/contents_manifesto.ignore.php';
+
+
+	/**
+	 * commands
+	 *
+	 * Pickles2 が認識するコマンドのパスを設定します。
+	 * コマンドのパスが通っていない場合は、絶対パスで設定してください。
+	 */
+	$conf->commands = new stdClass;
+	$conf->commands->php = 'php';
+
+	/** php.ini のパス。主にパブリッシュ時のサブクエリで使用する。 */
+	$conf->path_phpini = null;
+
+
+	/**
+	 * directory index
+	 *
+	 * `directory_index` は、省略できるファイル名のリストを設定します。
+	 * 複数指定可能です。
+	 *
+	 * この一覧にリストされたファイル名に対するリンクは、ファイル名なしのURLと同一視されます。
+	 * ファイル名が省略されたアクセス(末尾が `/` の場合)に対しては、
+	 * 最初のファイル名と同じものとして処理します。
+	 */
 	$conf->directory_index = array(
 		'index.html'
 	);
 
 
-	// system
-	$conf->file_default_permission = '775';
-	$conf->dir_default_permission = '775';
-	$conf->filesystem_encoding = 'UTF-8';
-	$conf->output_encoding = 'UTF-8';
-	$conf->output_eol_coding = 'lf';
-	$conf->session_name = 'PXSID';
-	$conf->session_expire = 1800;
-	$conf->allow_pxcommands = 1; // PX Commands のウェブインターフェイスからの実行を許可
-	$conf->default_timezone = 'Asia/Tokyo';
-
-	// commands
-	$conf->commands = new stdClass;
-	$conf->commands->php = 'php';
-	$conf->path_phpini = null; // php.ini のパス。主にパブリッシュ時のサブクエリで使用する。
-
-	// processor
+	/**
+	 * paths_proc_type
+	 *
+	 * パスのパターン別に処理方法を設定します。
+	 *
+	 * - ignore = 対象外パス。Pickles 2 のアクセス可能範囲から除外します。このパスにへのアクセスは拒絶され、パブリッシュの対象からも外されます。
+	 * - direct = 物理ファイルを、そのまま無加工で出力します。 (デフォルト)
+	 * - その他 = extension名
+	 *
+	 * パターンは先頭から検索され、はじめにマッチした設定を採用します。
+	 * ワイルドカードとして "*"(アスタリスク) が使用可能です。
+	 *
+	 * extensionは、 `$conf->funcs->processor` に設定し、設定した順に実行されます。
+	 * 例えば、 '*.html' => 'html' にマッチしたリクエストは、
+	 * $conf->funcs->processor->html に設定したプロセッサのリストに沿って、上から順に処理されます。
+	 */
 	$conf->paths_proc_type = array(
-		// パスのパターン別に処理方法を設定
-		//     - ignore = 対象外パス
-		//     - direct = 加工せずそのまま出力する(デフォルト)
-		//     - その他 = extension 名
-		// パターンは先頭から検索され、はじめにマッチした設定を採用する。
-		// ワイルドカードとして "*"(アスタリスク) を使用可。
 		'/.htaccess' => 'ignore' ,
 		'/.travis.yml' => 'ignore' ,
 		'/appveyor.yml' => 'ignore' ,
@@ -80,12 +114,40 @@ return call_user_func( function(){
 	);
 
 
+
+	// system
+
+	/** ファイルに適用されるデフォルトのパーミッション */
+	$conf->file_default_permission = '775';
+	/** ディレクトリに適用されるデフォルトのパーミッション */
+	$conf->dir_default_permission = '775';
+	/** ファイルシステムの文字セット。ファイル名にマルチバイト文字を使う場合に参照されます。 */
+	$conf->filesystem_encoding = 'UTF-8';
+	/** 出力文字エンコーディング名 */
+	$conf->output_encoding = 'UTF-8';
+	/** 出力改行コード名 (cr|lf|crlf) */
+	$conf->output_eol_coding = 'lf';
+	/** セッション名 */
+	$conf->session_name = 'PXSID';
+	/** セッションの有効期間 */
+	$conf->session_expire = 1800;
+	/** PX Commands のウェブインターフェイスからの実行を許可 */
+	$conf->allow_pxcommands = 0;
+	/** タイムゾーン */
+	$conf->default_timezone = 'Asia/Tokyo';
+
+
+
 	// -------- functions --------
 
 	$conf->funcs = new stdClass;
 
-	// funcs: Before sitemap
-	$conf->funcs->before_sitemap = [
+	/**
+	 * funcs: Before sitemap
+	 *
+	 * サイトマップ読み込みの前に実行するプラグインを設定します。
+	 */
+	$conf->funcs->before_sitemap = array(
 		// PX=clearcache
 		'picklesFramework2\commands\clearcache::register' ,
 
@@ -95,10 +157,14 @@ return call_user_func( function(){
 		 // PX=phpinfo
 		'picklesFramework2\commands\phpinfo::register' ,
 
-	];
+	);
 
-	// funcs: Before content
-	$conf->funcs->before_content = [
+	/**
+	 * funcs: Before content
+	 *
+	 * サイトマップ読み込みの後、コンテンツ実行の前に実行するプラグインを設定します。
+	 */
+	$conf->funcs->before_content = array(
 		// PX=api
 		'picklesFramework2\commands\api::register' ,
 
@@ -111,13 +177,21 @@ return call_user_func( function(){
 			)
 		)).')' ,
 
-	];
+	);
 
 
-	// processor
+	/**
+	 * processor
+	 *
+	 * コンテンツの種類に応じた加工処理の設定を行います。
+	 * `$conf->funcs->processor->{$paths_proc_typeに設定した処理名}` のように設定します。
+	 * それぞれの処理は配列で、複数登録することができます。処理は上から順に実行されます。
+	 *
+	 * Tips: テーマは、html に対するプロセッサの1つとして実装されています。
+	 */
 	$conf->funcs->processor = new stdClass;
 
-	$conf->funcs->processor->html = [
+	$conf->funcs->processor->html = array(
 		// ページ内目次を自動生成する
 		'picklesFramework2\processors\autoindex\autoindex::exec' ,
 
@@ -129,50 +203,77 @@ return call_user_func( function(){
 
 		// output_encoding, output_eol_coding の設定に従ってエンコード変換する。
 		'picklesFramework2\processors\encodingconverter\encodingconverter::exec' ,
-	];
+	);
 
-	$conf->funcs->processor->css = [
+	$conf->funcs->processor->css = array(
 		// output_encoding, output_eol_coding の設定に従ってエンコード変換する。
 		'picklesFramework2\processors\encodingconverter\encodingconverter::exec' ,
-	];
+	);
 
-	$conf->funcs->processor->js = [
+	$conf->funcs->processor->js = array(
 		// output_encoding, output_eol_coding の設定に従ってエンコード変換する。
 		'picklesFramework2\processors\encodingconverter\encodingconverter::exec' ,
-	];
+	);
 
-	$conf->funcs->processor->md = [
+	$conf->funcs->processor->md = array(
 		// Markdown文法を処理する
 		'picklesFramework2\processors\md\ext::exec' ,
 
-		// html の処理を追加
+		// html のデフォルトの処理を追加
 		$conf->funcs->processor->html ,
-	];
+	);
 
-	$conf->funcs->processor->scss = [
+	$conf->funcs->processor->scss = array(
 		// SCSS文法を処理する
 		'picklesFramework2\processors\scss\ext::exec' ,
 
-		// css の処理を追加
+		// css のデフォルトの処理を追加
 		$conf->funcs->processor->css ,
-	];
+	);
 
 
-	// funcs: Before output
-	$conf->funcs->before_output = [
-	];
+	/**
+	 * funcs: Before output
+	 *
+	 * 最終出力の直前で実行される処理を設定します。
+	 * この処理は、拡張子によらずすべてのリクエストが対象です。
+	 * (HTMLの場合は、テーマの処理の後のコードが対象になります)
+	 */
+	$conf->funcs->before_output = array(
+	);
+
+
 
 	// -------- PHP Setting --------
 
-	// [memory_limit]
-	// PHPのメモリの使用量の上限を設定します。
-	// 正の整数値で上限値(byte)を与えます。
-	//     例: 1000000 (1,000,000 bytes)
-	//     例: "128K" (128 kilo bytes)
-	//     例: "128M" (128 mega bytes)
-	// -1 を与えた場合、無限(システムリソースの上限まで)に設定されます。
-	// サイトマップやコンテンツなどで、容量の大きなデータを扱う場合に調整してください。
+	/**
+	 * `memory_limit`
+	 *
+	 * PHPのメモリの使用量の上限を設定します。
+	 * 正の整数値で上限値(byte)を与えます。
+	 *
+	 *     例: 1000000 (1,000,000 bytes)
+	 *     例: "128K" (128 kilo bytes)
+	 *     例: "128M" (128 mega bytes)
+	 *
+	 * -1 を与えた場合、無限(システムリソースの上限まで)に設定されます。
+	 * サイトマップやコンテンツなどで、容量の大きなデータを扱う場合に調整してください。
+	 */
 	// @ini_set( 'memory_limit' , -1 );
+
+	/**
+	 * `display_errors`, `error_reporting`
+	 *
+	 * エラーを標準出力するための設定です。
+	 *
+	 * PHPの設定によっては、エラーが発生しても表示されない場合があります。
+	 * もしも、「なんか挙動がおかしいな？」と感じたら、
+	 * 必要に応じてこれらのコメントを外し、エラー出力を有効にしてみてください。
+	 *
+	 * エラーメッセージは問題解決の助けになります。
+	 */
+	// @ini_set('display_errors', 'On');
+	// @ini_set('error_reporting', 32767);
 
 
 	return $conf;
