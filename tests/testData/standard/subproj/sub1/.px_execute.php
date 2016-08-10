356f096c48c2669a5d10c3e7ca976dd529b6b31a
Tomoya Koyanagi <tomk79@gmail.com>
2016-08-10 10:33:52 +0900

    $_SERVER['DOCUMENT_ROOT'] を扱うテスト追加
    - ※未対応のテストパターンなのでエラーになります

---
+++ tests/testData/standard/subproj/sub1/.px_execute.php
@@ -0,0 +1,6 @@
+<?php
+chdir(__DIR__);
+@ini_set('display_errors', 1);
+@ini_set('error_reporting', E_ALL);
+require_once( '../../../../../vendor/autoload.php' );
+new picklesFramework2\pickles('./px-files/');
