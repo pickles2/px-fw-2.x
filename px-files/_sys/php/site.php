636c10f5771c2d40955316803ef11dc0e5faffd4
Tomoya Koyanagi <tomk79@gmail.com>
2016-11-30 21:15:38 +0900

    Windowsサーバーで、サイトマップキャッシュが排他ロックされて更新に失敗することがある問題を修正。

--- px-files/_sys/php/site.php
+++ px-files/_sys/php/site.php
@@ -69,8 +69,12 @@ class site{
 			$tmp_path_cache = realpath($tmp_path_cache).DIRECTORY_SEPARATOR;
 			clearstatcache();
 			try {
+				$sitemap_sqlite_filename = @include($tmp_path_cache.'sitemap.sqlite.filename.array');
+				if(!strlen( $sitemap_sqlite_filename )){
+					$sitemap_sqlite_filename = 'sitemap.sqlite';
+				}
 				$this->pdo = new \PDO(
-					'sqlite:'.$tmp_path_cache.'sitemap.sqlite',
+					'sqlite:'.$tmp_path_cache.$sitemap_sqlite_filename,
 					null, null,
 					array(
 						\PDO::ATTR_PERSISTENT => false, // ←これをtrueにすると、"持続的な接続" になる
@@ -195,7 +199,11 @@ class site{
 				}
 
 				clearstatcache();
-				if( !$this->px->fs()->is_file( $path_sitemap_cache_dir.'sitemap.sqlite' ) || !filesize($path_sitemap_cache_dir.'sitemap.sqlite') ){
+				$sitemap_sqlite_filename = @include($tmp_path_cache.'sitemap.sqlite.filename.array');
+				if(!strlen( $sitemap_sqlite_filename )){
+					$sitemap_sqlite_filename = 'sitemap.sqlite';
+				}
+				if( !$this->px->fs()->is_file( $path_sitemap_cache_dir.$sitemap_sqlite_filename ) || !filesize($path_sitemap_cache_dir.$sitemap_sqlite_filename) ){
 					// サイトマップキャッシュ生成が不完全な状態でPDOでサイトマップの操作をしようとすると、
 					// Fatal Error が発生する場合があるため、使えないようにしておく。
 					//
@@ -518,7 +526,7 @@ INSERT INTO sitemap(
 			$i = 0;
 			while( !$this->px->fs()->copy(
 				$path_sitemap_cache_dir.'sitemap.sqlite.tmp',
-				$path_sitemap_cache_dir.'sitemap.sqlite'
+				$path_sitemap_cache_dir.'sitemap'.$i.'.sqlite'
 			) ){
 				$i ++;
 				if( $i > 10 ){
@@ -530,7 +538,7 @@ INSERT INTO sitemap(
 
 			// PDO をリロード
 			$this->pdo = new \PDO(
-				'sqlite:'.$path_sitemap_cache_dir.'sitemap.sqlite',
+				'sqlite:'.$path_sitemap_cache_dir.'sitemap'.$i.'.sqlite',
 				null, null,
 				array(
 					\PDO::ATTR_PERSISTENT => false, // ←これをtrueにすると、"持続的な接続" になる
@@ -539,6 +547,7 @@ INSERT INTO sitemap(
 
 			// remove tmp database
 			@$this->px->fs()->rm( $path_sitemap_cache_dir.'sitemap.sqlite.tmp' );
+			$this->px->fs()->save_file( $path_sitemap_cache_dir.'sitemap.sqlite.filename.array' , self::data2phpsrc('sitemap'.$i.'.sqlite') );
 		}
 		$this->px->fs()->save_file( $path_sitemap_cache_dir.'sitemap.array' , self::data2phpsrc($this->sitemap_array) );
 		$this->px->fs()->save_file( $path_sitemap_cache_dir.'sitemap_id_map.array' , self::data2phpsrc($this->sitemap_id_map) );
